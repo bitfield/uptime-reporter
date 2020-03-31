5f11751103d0de460eb47a9115155cab2b375861
John Arundel <john@bitfieldconsulting.com>
2020-03-31 16:43:45 +0100

    First cut of analysis program

--- cmd/analyse/main.go
+++ cmd/analyse/main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	reporter "github.com/bitfield/uptime-reporter"
+	"github.com/montanaflynn/stats"
 )
 
 func main() {
-	token := os.Getenv("UPTIME_API_TOKEN")
-	if token == "" {
-		log.Fatal("no UPTIME_API_TOKEN set")
-	}
-	r, err := reporter.New(token)
-	if err != nil {
-		log.Fatal(err)
-	}
 	sites, err := reporter.ReadCSV(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	outages := map[string]stats.Float64Data{}
+	downtimes := map[string]stats.Float64Data{}
+	for _, s := range sites {
+		outages[s.Sector] = append(outages[s.Sector], float64(s.Outages))
+		outages["All"] = append(outages["All"], float64(s.Outages))
+		downtimes[s.Sector] = append(downtimes[s.Sector], float64(s.DowntimeSecs))
+		downtimes["All"] = append(downtimes["All"], float64(s.DowntimeSecs))
+	}
+	for sector := range outages {
+		fmt.Printf("Sector: %s (%d sites)\n", sector, len(outages[sector]))
+		outageSummary, err := reporter.StatsSummary(outages[sector])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Outages: %s\n", outageSummary)
+		downtimeSummary, err := reporter.StatsSummary(downtimes[sector])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Downtimes: %s\n", downtimeSummary)
+		fmt.Println()
+	}
 }
